918cd6239560e2df2d7ae10f1effb1bd26d85c20
Michael R Gelinas <m.roygelinas@outlook.com>
2026-01-28 13:48:51 -0500

    replace missing age data with NaN

--- titanic.py
+++ titanic.py
@@ -24,3 +24,10 @@ survivors_by_sex = df.groupby('Sex')['Survived'].sum()
 
 survivors_by_age = df.groupby("Age")['Survived'].sum()
 #print("Survivors by Age:\n", survivors_by_age)
+
+# -----------------------------------------------------------------------------
+# Analyze survival rates by age groups -> create age groups
+# -----------------------------------------------------------------------------
+
+# Replace \N with NaN
+df['Age'] = df['Age'].replace('\\N', np.nan)
