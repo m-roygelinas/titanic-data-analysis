3c714728f6c98eca519b400dee6df3488af9483a
Michael R Gelinas <m.roygelinas@outlook.com>
2026-01-28 13:17:07 -0500

    imports and adding CSV file

---
+++ titanic.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+
+df = pd.read_csv("C:\\Users\\acouder\\OneDrive - cssenergie.gouv.qc.ca\\Michael\\apps\\business_operations_survey\\business-operations-survey-2022-information-and-communications-technology.csv")
+
+print(df.head())  
+print(df.info())
