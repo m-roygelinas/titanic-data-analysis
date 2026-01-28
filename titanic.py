bd179a77013b1c209e51bfc81efa942e118fd21a
Michael R Gelinas <m.roygelinas@outlook.com>
2026-01-28 13:44:16 -0500

    basic statistics

--- titanic.py
+++ titanic.py
@@ -5,5 +5,22 @@ import datetime as dt
 
 df = pd.read_csv("titanic.csv")
 
+# -----------------------------------------------------------------------------
+# Get basic info about the dataframe
+# -----------------------------------------------------------------------------
+
 print(df.head())  
 print(df.info())
+
+# -----------------------------------------------------------------------------
+# Basic statistics
+# -----------------------------------------------------------------------------
+
+survivors_by_class = df.groupby('Pclass')['Survived'].sum()
+#print("Survivors by Class:\n", survivors_by_class)
+
+survivors_by_sex = df.groupby('Sex')['Survived'].sum()
+#print("Survivors by Sex:\n", survivors_by_sex)
+
+survivors_by_age = df.groupby("Age")['Survived'].sum()
+#print("Survivors by Age:\n", survivors_by_age)
